ce2f5a74e052e6a554215c7441c2cf5f8cc6ca47
SarahEmmy <bawahsarah@gmail.com>
2023-06-05 08:57:23 -0700

    updates

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 98 + (a ** b)
-    return f"The result is: {result}"
+    return (a ** b) + 98
--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-print(f"Value of number: {number:.2f}")
+print("Float: {:03.2f}".format(number))
