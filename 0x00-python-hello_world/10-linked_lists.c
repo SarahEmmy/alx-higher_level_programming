728159509c18c9dea3b542ecf9fe1976c272445d
SarahEmmy <bawahsarah@gmail.com>
2023-06-05 07:56:48 -0700

    Updates

--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -9,40 +9,41 @@
  */
 size_t print_listint(const listint_t *h)
 {
-  const listint_t *current;
-  unsigned int n; /* number of nodes */
-  
-  current = h;
-  n = 0;
-  while (current != NULL)
-  {
-    printf("%i\n", current->n);
-    current = current->next;
-    n++;
-  }
-  
-  return (n);
+	const listint_t *current;
+	unsigned int n; /* number of nodes */
+
+	current = h;
+	n = 0;
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
+	}
+
+	return (n);
 }
 
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list
+ * add_nodeint - adds a new node at the beginning of a
+ * listint_t list
  * @head: pointer to a pointer of the start of the list
  * @n: integer to be included in node
  * Return: address of the new element or NULL if it fails
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-  listint_t *new;
-  
-  new = malloc(sizeof(listint_t));
-  if (new == NULL)
-    return (NULL);
-  
-  new->n = n;
-  new->next = *head;
-  *head = new;
-  
-  return (new);
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
 
 /**
@@ -52,12 +53,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
  */
 void free_listint(listint_t *head)
 {
-  listint_t *current;
-  
-  while (head != NULL)
-  {
-    current = head;
-    head = head->next;
-    free(current);
-  }
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
 }
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -9,13 +9,14 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
+	int n;
+	struct listint_s *next;
+}
+
+listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
