d23bffeee70e357790ac4168e60e5136692d954b
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-06-06 15:31:20 +0100

    Update lists.h

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -4,13 +4,13 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- * 
- * Description: singly linked list node structure
- * for Holberton project
- */
+
+struct listint_s - Singly linked list
+@n: Integer value stored in the node
+@next: Pointer to the next node
+Description: Node structure for a singly linked list
+used in ALX project
+*/
 typedef struct listint_s
 {
 	    int n;
