92ddfa84e331c5007867642727dbcf2848ce24cb
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-07-03 18:56:08 +0100

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,65 +1,54 @@
 #!/usr/bin/python3
-"""
-    Module containing functions to search for solutions to N-queens problem.
+"""N-Queens Problem Solver Module
+This module contains functions to search for solutions to the N-Queens problem.
 """
 
 
 def all_possible(n=4):
-    """ Function to find all possible solutions by placing the first queen on
-        the first row, with different column positions starting from 2nd
-        column to 2nd to last column.
-        Args:
-            n (int): The size of the chess board, but also the number of queens
-                to place on the board.
+    """ Functions:is_safe(row, col, board): 
+    Check if placing a queen at the given position (row, col) on the board is safe.
+    solve_n_queens(n): Solve the N-Queens problem and return a list of all possible solutions for a board of size n x n.
     """
 
-    for i in range(n):
+    for x in range(n):
         matrix = []
-        matrix.append([0, i])
+        matrix.append([0, x])
         col = [z for z in range(n)]
-        col.remove(i)
+        col.remove(x)
         recur_bt(matrix, 1, col, n)
 
 
 def recur_bt(matrix, row, col, n):
     """
-        Recursive backtrack function. Test remaining possible positions using
-        `row` and `col` values in order to place another queen on the board.
-        Base case, either the length of `matrix` is the same as `n` (all queens
-        have been placed in non threatening positions) or all possible
-        combinations of `row` and `col` with the current matrix yields no
-        solution.
-        Args:
-            matrix (:obj:`list` of :obj:`list` or int): A matrix list of lists
-                representing the positions of queens placed on the board.
-            row (:obj:`list` of int): Possible row positions the remaining
-                queens can be placed on.
-            col (:obj:`list` of int): Possible column positions the remaining
-                queens can be placed on.
-            n (int): The size of the board, but also the number of queens that
-                can fit.
-        Returns:
-            A list of lists which is a solution, or None if the current members
-            of the matrix lead to a dead end.
+    Recursive Backtrack Function - Recursive this function is used to test 
+    remaining possible positions for placing another queen on the board.
+    It operates recursively and serves as the main algorithm for solving 
+    the N-Queens problem. Parameters:
+    matrix: A list of lists representing the positions of queens placed on the board.
+    row: A list of possible row positions where the remaining queens can be placed.
+    col: A list of possible column positions where the remaining queens can be placed.
+    n: The size of the board and the number of queens that can be placed.
+    Returns: A list of lists representing a valid solution if found, or 
+    None if the current configuration leads to a dead end.
     """
     if len(matrix) is n:
         return matrix
 
     if row:
-        i = row
-        for j in col:
-                if (not bot_right(matrix, i + 1, j + 1, n) or
-                    not bot_left(matrix, i + 1, j - 1, n) or
-                    not top_left(matrix, i - 1, j - 1, n) or
-                        not top_right(matrix, i - 1, j + 1, n)):
+        x = row
+        for y in col:
+                if (not bot_right(matrix, x + 1, y + 1, n) or
+                    not bot_left(matrix, x + 1, y - 1, n) or
+                    not top_left(matrix, x - 1, y - 1, n) or
+                        not top_right(matrix, x - 1, y + 1, n)):
                         continue
-                matrix.append([i, j])
+                matrix.append([x, y])
                 new_col = list(col)
-                new_col.remove(j)
+                new_col.remove(y)
                 new_matrix = recur_bt(matrix, row + 1, new_col, n)
                 if new_matrix is not None:
                     print(matrix)
-                matrix.remove([i, j])
+                matrix.remove([x, y])
     return None
 
 
@@ -74,7 +63,7 @@ def bot_right(matrix, y, x, n):
             x (int): "column" or x co-ordinate.
             n (int): The size of the board.
         Returns:
-            True if no queens exist on the bottom right diagonal otherwise
+            True if no queens exist on the bottom right diagonal of the given position, otherwise
             False.
     """
     while y < n and x < n:
