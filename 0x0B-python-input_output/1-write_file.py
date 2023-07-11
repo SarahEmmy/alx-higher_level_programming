772d2c1fa68380a1fe3c74e25ed92a710e266042
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-07-11 18:48:34 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Write to a file """
+""" Write to a txt file """
 
 
 def write_file(filename="", text=""):
-    """ Writes a string to a text file (UTF8) and returns
+    """ Writes a string to a txt file (UTF8) and returns
     the number of characters written """
     with open(filename, 'w', encoding='utf-8') as f:
         return (f.write(text))
