a63e7cd89d8a3f9b17c1dae8a02da5f7ebc86c2e
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-08-15 21:27:04 +0100

    Update 1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,4 @@
+-- We're creating a new world of data, but only if it hasn't been born yet
 -- Create a database
 -- Create a database if it does not exists
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
