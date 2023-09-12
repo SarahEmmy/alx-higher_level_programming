85fcbcbd4dfe5d9eba04bf140a38422bb8b0a61a
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-09-12 21:13:18 +0100

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,29 +1,29 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.w = width;
+      this.h = height;
     }
   }
 
   print () {
-    let i = 0;
-    for (i; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+    let j = 0;
+    for (j; j< this.h; j++) {
+      console.log('X'.repeat(this.w));
     }
   }
 
   rotate () {
-    const tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
+    const tmp = this.w;
+    this.w = this.h;
+    this.h = tmp;
   }
 
   double () {
-    this.width *= 2;
-    this.height *= 2;
+    this.w *= 2;
+    this.h *= 2;
   }
 }
 module.exports = Rectangle;
