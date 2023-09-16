f20affde98f0f9cfd7683b6d6bf80649273df104
SarahEmmy <122871970+SarahEmmy@users.noreply.github.com>
2023-09-17 00:46:29 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
+""" write a Script that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 from sys import argv
 
-# The code should not be executed when imported
+# This code should not be executed when imported
 if __name__ == '__main__':
 
-    # make a connection to the database
+    # this is a connection to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
