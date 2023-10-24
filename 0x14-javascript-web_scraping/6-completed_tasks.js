9dcec5420f6dccbf9a0fa750538e2e8a7a19403c
SarahB <122871970+SarahEmmy@users.noreply.github.com>
2023-10-24 13:06:15 +0100

    Add files via upload

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,21 +2,23 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(erorr);
-  } 
-  if (response.statusCode === 200) {
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
     const completed = {};
     const tasks = JSON.parse(body);
-    tasks.forEach((task => {
+    tasks.forEach(task => {
       if (task.completed === true) {
-        completed[task.userId] = (completed[task.userId] || 0) + 1;
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
       }
     });
     console.log(completed);
-  } 
-  else {
-    console.error(`An error occurred. Status code: ${response.statusCode}`);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
